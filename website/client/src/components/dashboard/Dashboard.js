6ed158aec0163662fe9f3328d22c1167bccfac4b
12dash <soham.dandapath@gmail.com>
2020-12-20 01:55:39 +0800

    Initial Commit

--- website/client/src/components/dashboard/Dashboard.js
+++ website/client/src/components/dashboard/Dashboard.js
@@ -2,48 +2,54 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import Chat from '../chat';
+
 class Dashboard extends Component {
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
   };
-render() {
-  const { user } = this.props.auth;
-  return (
-    <div className="container valign-wrapper">
-      <div className="row">
-        <div className="col s12 center-align">
-          <h4>
-            <b>Hey there,</b> {user.name.split(" ")[0]}
-            <p className="flow-text grey-text text-darken-1">
-              You are now logged in!
-              </p>
-          </h4>
-          <iframe
-            allow="microphone;"
-            width="500"
-            height="430"
-            src="https://console.dialogflow.com/api-client/demo/embedded/b5c7a279-13b0-4a2d-922f-ce91adfb88b5">
-          </iframe>
 
-          <br />
-          <button
-            style={{
-              width: "150px",
-              borderRadius: "3px",
-              letterSpacing: "1.5px",
-              marginTop: "1rem"
-            }}
-            onClick={this.onLogoutClick}
-            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-          >
-            Logout
+  componentDidMount() {
+    (function (d, m) {
+      var kommunicateSettings = { "appId": "1ba12c38881635552a178a7b7869a03e6" , "popupWidget": true, "automaticChatOpenOnNavigation": true };
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  }
+  render() {
+    const { user } = this.props.auth;
+    return (
+      <div className="container valign-wrapper">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4>
+              <b>Hey there,</b> {user.name.split(" ")[0]}
+              <p className="flow-text grey-text text-darken-1">
+                You are now logged in!
+              </p>
+            </h4>            
+          <Chat/>
+            <br />
+            <button
+              style={{
+                width: "150px",
+                borderRadius: "3px",
+                letterSpacing: "1.5px",
+                marginTop: "1rem"
+              }}
+              onClick={this.onLogoutClick}
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            >
+              Logout
             </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 Dashboard.propTypes = {
   logoutUser: PropTypes.func.isRequired,
