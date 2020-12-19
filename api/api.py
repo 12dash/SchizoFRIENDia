0db9aa755c797e9389eca12622219b7e3b0c6c6e
12dash <soham.dandapath@gmail.com>
2020-12-20 02:39:21 +0800

    Updates

---
+++ api/api.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from inference import Predict 
+
+import base64
+import io
+from PIL import Image
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return "Backend Server"
+
+@app.route("/predict")
+def predict():
+    img = request.files['file']
+    img = base64.b64decode(img)
+    buf = io.BytesIO(img)
+    img = Image.open(buf)
+    img =  base64.b64encode(Predict().output)    
+    return img
+
+if __name__ == "__main__":
+    app.run(debug=True, port = 5001)
+
+CORS(app, expose_headers='Authorization')
