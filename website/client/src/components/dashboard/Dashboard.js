3d8dfc13d317d87619e5b44d3bc558dd26cc42c8
12dash <soham.dandapath@gmail.com>
2020-12-20 12:44:37 +0800

    UI

--- website/client/src/components/dashboard/Dashboard.js
+++ website/client/src/components/dashboard/Dashboard.js
@@ -6,60 +6,76 @@ import axios from 'axios';
 import Chat from './chat';
 
 class Dashboard extends Component {
-    constructor(props) {
-        super(props);
-          this.state = {
-            selectedFile: null
-          }
-       
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null
+    }
+  }
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
   };
   componentDidMount() {
-    (function(d, m){
-        var kommunicateSettings = 
-            {"appId":"1ba12c38881635552a178a7b7869a03e6","popupWidget":true,"automaticChatOpenOnNavigation":true};
-        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-        window.kommunicate = m; m._globals = kommunicateSettings;
+    (function (d, m) {
+      var kommunicateSettings =
+        { "appId": "1ba12c38881635552a178a7b7869a03e6", "popupWidget": true, "automaticChatOpenOnNavigation": true };
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      window.kommunicate = m; m._globals = kommunicateSettings;
     })(document, window.kommunicate || {});
   }
-  onChangeHandler=event=>{
+  onChangeHandler = event => {
     this.setState({
       selectedFile: event.target.files[0],
       loaded: 0,
     })
   }
   onClickHandler = () => {
-    const data = new FormData() 
+    const data = new FormData()
     data.append('file', this.state.selectedFile)
     axios.post("http://localhost:5001/predict", data, { // receive two parameter endpoint url ,form data 
-      })
+    })
       .then(res => { // then print response status
         console.log(res)
         console.log(res.statusText)
         const img = res.data;
-        this.setState({base64File: 'data:image/png;base64,'+img});    
+        this.setState({ base64File: 'data:image/png;base64,' + img });
       })
       .catch(err => {
         console.log(err)
       })
-}
-render() {
+  }
+  render() {
     const { user } = this.props.auth;
     return (
       <div className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
-            <div className="form-group files">
-                <label>Upload Your File</label>
-                <input type="file" className="form-control" name="file" onChange={this.onChangeHandler}></input>
-                <button style={{margin: "0 auto"}} type="button" className="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
-                {this.state.base64File ? <img src = {(this.state.base64File)}/>: ''}
+            <div className="flow-text grey-text text-darken-1">
+              Hello, let us first give  the voices in your head a face.
             </div>
+            <div className="form-group files">
+              <label>Upload Your File</label>
+              <input type="file" className="form-control" name="file" onChange={this.onChangeHandler}></input>
+              <br />
+              <button style={{ margin: "0 auto" }} type="button" className="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
+            </div>
+            <br />
+            {
+              this.state.base64File ?
+                <div className="row">
+                  <div className="col center-align">
+                    <img src={this.state.base64File} />
+                  </div>
+                  <div className="col-1 flow-text grey-text text-darken-1 left-align">
+                    See that's the person who is the voice in the head.
+                    <br/> Its as blurry as the voices. 
+                  </div>
+                </div>
+                : <></>
+            }
             <button
               style={{
                 width: "150px",
@@ -68,8 +84,7 @@ render() {
                 marginTop: "1rem"
               }}
               onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
+              className="btn btn-large waves-effect waves-light hoverable blue accent-3" >
               Logout
             </button>
           </div>
