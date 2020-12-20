afd973f494bac5414dbe83fb291181d6ca94569d
12dash <soham.dandapath@gmail.com>
2020-12-20 11:46:28 +0800

    Updates for Chat file

--- website/client/src/components/dashboard/Dashboard.js
+++ website/client/src/components/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import axios from 'axios';
-import Chat from '../chat';
+import Chat from './chat';
 
 class Dashboard extends Component {
     constructor(props) {
